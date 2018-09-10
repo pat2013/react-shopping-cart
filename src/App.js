95cc4e7dcc4412d8d5afbca173db311eb581fb21
pat2013 <patrick.yu.2017@gmail.com>
2018-09-10 09:51:45 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>my shopping cart</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
